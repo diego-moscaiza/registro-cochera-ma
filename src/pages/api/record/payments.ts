862eb06cccbc84c454878204a554e48e8b9657f8
Diego Moscaiza <diego-moscaiza@outlook.com>
2025-03-23 20:19:14 -0500

    fix: Indentation and empty spaces have been tidied up

--- src/pages/api/record/payments.ts
+++ src/pages/api/record/payments.ts
@@ -2,41 +2,40 @@ import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
 
 export const GET: APIRoute = async () => {
-    const { data, error } = await supabase
-        .from("pagos_diarios")
-        .select("*")
-        .order("created_at", { ascending: false });
+	const { data, error } = await supabase
+		.from("pagos_diarios")
+		.select("*")
+		.order("created_at", { ascending: false });
 
-    if (error || !data) {
-        return new Response(
-            JSON.stringify({
-                error: error?.message || "No se encontraron datos.",
-            }),
-            { status: 500, headers: { "Content-Type": "application/json" } }
-        );
-    }
+	if (error || !data) {
+		return new Response(
+			JSON.stringify({
+				error: error?.message || "No se encontraron datos.",
+			}),
+			{ status: 500, headers: { "Content-Type": "application/json" } }
+		);
+	}
 
-    return new Response(JSON.stringify(data ?? []), {
-        headers: { "Content-Type": "application/json" },
-    });
+	return new Response(JSON.stringify(data ?? []), {
+		headers: { "Content-Type": "application/json" },
+	});
 };
 
-
 export const POST: APIRoute = async ({ request }) => {
-    const { dueno_id, nombre_pagador } = await request.json();
-    const { data, error } = await supabase
-        .from("pagos_diarios")
-        .insert({ dueno_id, nombre_pagador })
-        .select();
+	const { dueno_id, nombre_pagador } = await request.json();
+	const { data, error } = await supabase
+		.from("pagos_diarios")
+		.insert({ dueno_id, nombre_pagador })
+		.select();
 
-    if (error) {
-        return new Response(
-            JSON.stringify({
-                error: error.message,
-            }),
-            { status: 500 },
-        );
-    }
+	if (error) {
+		return new Response(
+			JSON.stringify({
+				error: error.message,
+			}),
+			{ status: 500 },
+		);
+	}
 
-    return new Response(JSON.stringify(data));
+	return new Response(JSON.stringify(data));
 };
