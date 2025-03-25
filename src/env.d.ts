9e21cdc21b37a1a172850edab0442eabcf62cedd
Diego Moscaiza <diego-moscaiza@outlook.com>
2025-03-25 00:29:25 -0500

    feat: Add new fields to the Locals interface in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,18 @@
 /// <reference types="astro/client" />
 declare namespace App {
-  interface Locals {
-    email: string;
-  }
+	interface Locals {
+		email: string;
+		displayName: string;
+		phone: string;
+		otherInfo: any;
+	}
 }
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string;
-  readonly SUPABASE_ANON_KEY: strinq;
+	readonly SUPABASE_URL: string;
+	readonly SUPABASE_ANON_KEY: strinq;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+	readonly env: ImportMetaEnv;
 }
